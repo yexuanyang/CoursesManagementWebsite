740a9b8a0bfea66ed61fd57b4d2605fe51e38807
yyx <1182282462@qq.com>
2022-05-19 16:15:52 +0800

    考试时间提醒以及课外活动开始提醒

--- static/assets/js/time.js
+++ static/assets/js/time.js
@@ -42,20 +42,35 @@ var chtoInt = {
 
 var in_course_time = document.getElementsByClassName("courseTime");
 // console.log(in_course_time);
-console.log(parseInt(in_course_time[1].innerHTML.substring(3, 5)));
-console.log(parseInt(in_course_time[1].innerHTML.substring(6, 8)));
-console.log(parseInt(in_course_time[1].innerHTML.substring(9, 11)));
-console.log(parseInt(in_course_time[1].innerHTML.substring(12)));
+// console.log(parseInt(in_course_time[1].innerHTML.substring(3, 5)));
+// console.log(parseInt(in_course_time[1].innerHTML.substring(6, 8)));
+// console.log(parseInt(in_course_time[1].innerHTML.substring(9, 11)));
+// console.log(parseInt(in_course_time[1].innerHTML.substring(12)));
 
 var course_name = document.getElementsByClassName("courseName");
+// console.log(course_name);
 
 var exam_time = document.getElementsByClassName("examTime");
+// console.log(exam_time[1]["outerText"]);
+// console.log(exam_time[1]["outerText"].substring(11,16));
+// console.log(exam_time[1]["outerText"].substring(29));
 
 var out_course_begin_time = document.getElementsByClassName("outCourseBeginTime");
+// console.log(out_course_begin_time[1]["outerText"]);
 var out_course_end_time = document.getElementsByClassName("outCourseEndTime");
+// console.log(out_course_end_time[1]["outerText"]);
 var out_course_date = document.getElementsByClassName("outCourseDate");
+// console.log(out_course_date[1]["outerText"]);
+console.log(out_course_date[1].innerHTML);
+var out_course_name = document.getElementsByClassName("outCourseName");
+console.log(out_course_name[1].innerHTML);
+
 var class_begin = false;
 var class_end = false;
+var out_course_begin = false;
+var out_course_end = false;
+var exam_start = false;
+var exam_end = false;
 
 function faketime() {
     if (parseInt(W) >= 7) {
@@ -97,22 +112,94 @@ function faketime() {
     data_list = [Y,MONTH,DAY,W, H, min, second];
     for (let i = 0; i < in_course_time.length; i++) {
         const inCourseTimeKey = in_course_time[i].innerHTML;
-        const day = chtoInt[inCourseTimeKey[2]];
-        if (day === parseInt(W)) {
+        const weekday = chtoInt[inCourseTimeKey[2]];
+        const examTimeKey = exam_time[i].innerHTML;
+        let startYear = parseInt(examTimeKey.substring(0,4));
+        let startMonth = parseInt(examTimeKey.substring(5,7));
+        let startDay = parseInt(examTimeKey.substring(8,10));
+        let startHour = parseInt(examTimeKey.substring(11,13));
+        let startMin = parseInt(examTimeKey.substring(14,16));
+        let endYear = parseInt(examTimeKey.substring(18,22));
+        let endMonth = parseInt(examTimeKey.substring(23,25));
+        let endDay = parseInt(examTimeKey.substring(26,28));
+        let endHour = parseInt(examTimeKey.substring(29,31));
+        let endMin = parseInt(examTimeKey.substring(32));
+        let endDate = examTimeKey.substring(18);
+        if (!exam_start &&startYear === parseInt(Y) && startMonth === parseInt(MONTH) && startDay ===parseInt(DAY) && startHour === parseInt(H) && startMin === parseInt(min) ){
+            exam_start = true;
+            exam_end = false;
+            class_begin = true;
+            class_end = true;
+            out_course_begin = true;
+            out_course_end = true;
+            window.alert( course_name[i].innerHTML + "考试" + "开始了\n结束时间："+ endDate );
+            break;
+        }
+        if(!exam_end && endYear === parseInt(Y) && endMonth === parseInt(MONTH) && endDay ===parseInt(DAY) && endHour === parseInt(H) && endMin === parseInt(min)){
+            exam_start = false;
+            exam_end = true;
+            class_begin = false;
+            class_end = false;
+            out_course_begin = false;
+            out_course_end = false;
+            window.alert( course_name[i].innerHTML + "考试" + "结束了" );
+            break;
+        }
+
+        if (weekday === parseInt(W)) {
             if (!class_begin && parseInt(H) === parseInt(inCourseTimeKey.substring(3, 5)) && parseInt(min) === parseInt(inCourseTimeKey.substring(6, 8))) {
                 window.alert(course_name[i].innerHTML + "开始上课了");
                 class_begin = true;
                 class_end = false;
+
+                //上课不能进行课外活动，把课外活动设置为已经开始
+                out_course_begin = true;
+                out_course_end = true;
                 break;
             }
             if (!class_end && parseInt(H) === parseInt(inCourseTimeKey.substring(9, 11) ) && parseInt(min) === parseInt(inCourseTimeKey.substring(12))) {
                 window.alert(course_name[i].innerHTML + "下课了");
                 class_end = true;
                 class_begin = false;
+                //下课了可以进行课外活动，把课外活动设置为未开始
+                out_course_begin = false;
+                out_course_end = false;
                 break;
             }
         }
+
+
     }
+
+    for (let i =0;i<out_course_date.length;i++){
+        const out_course_date_key = out_course_date[i].innerHTML;
+        const out_course_begin_time_key = out_course_begin_time[i].innerHTML;
+        const out_course_end_time_key = out_course_end_time[i].innerHTML;
+        let year = parseInt(out_course_date_key.substring(0,4));
+        let month = parseInt(out_course_date_key.substring(5,7));
+        let day = parseInt(out_course_date_key.substring(8));
+        let BeginTimeHour = parseInt(out_course_begin_time_key.substring(0,2));
+        let BeginTimeMin = parseInt(out_course_begin_time_key.substring(3));
+        let EndTimeHour = parseInt(out_course_end_time_key.substring(0,2));
+        let EndTimeMin = parseInt(out_course_end_time_key.substring(3));
+        if(year === parseInt(Y) && month ===parseInt(MONTH) && day ===parseInt((DAY))){
+            if(!out_course_begin &&  BeginTimeHour === parseInt(H) && BeginTimeMin === parseInt(min) ){
+                out_course_begin = true;
+                out_course_end = false;
+                window.alert("课外活动 "+out_course_name[i].innerHTML+" 开始了\n结束时间："+out_course_end_time_key);
+                break;
+            }
+            if(!out_course_end && EndTimeHour === parseInt(H) && EndTimeMin === parseInt(min) ){
+                out_course_begin = false;
+                out_course_end = true;
+                window.alert("课外活动 "+out_course_name[i].innerHTML+" 结束了");
+                break;
+            }
+
+        }
+
+    }
+
     var time_json = {
         time: JSON.stringify(data_list)
     };
